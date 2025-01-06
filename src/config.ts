5fe2cc73a039fd2cf5a5fdf2bdb0b104155cb580
啊茶kk <acgr18share@gmail.com>
2025-01-06 15:24:05 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -11,28 +11,28 @@ export const siteConfig: SiteConfig = {
   subtitle: '(゜-゜)つロ 干杯~',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345 /主题颜色的默认色调，从 0 到 360。红色：0，青色：200，青色：250，粉色：345
-    fixed: false,     // Hide the theme color picker for visitors /为访客隐藏主题颜色选择器
+    hue: 250,         // 主题颜色的默认色调，从 0 到 360。红色：0，青色：200，青色：250，粉色：345
+    fixed: false,     // 为访客隐藏主题颜色选择器
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: 'assets/images/demo-banner.png',   // 相对于/src目录。如果以“/”开头，则相对于 /public 目录
+    position: 'center',      // 相当于object-position，只支持'top'，'center'，'bottom'。默认为“中心”
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: false,         // 显示横幅图像的信用文本
+      text: '',              // 要显示的信用文本
+      url: ''                //（可选）原始艺术作品或艺术家页面的 URL 链接
     }
   },
   toc: {
-    enable: true,           // Display the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+    enable: true,           // 在帖子右侧显示目录
+    depth: 2                // 表中显示的最大标题深度，从 1 到 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [    // 将此数组留空以使用默认的图标
     // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    //   src: '/favicon/icon.png',    // 图标的路径，相对于 /public 目录
+    //   theme: 'light',              // （可选）“浅色”或“深色”，仅当您有不同的浅色和深色模式图标时才设置
+    //   sizes: '32x32',              // （可选）图标大小，仅当您有不同大小的图标时才设置
     // }
   ]
 }
@@ -44,23 +44,23 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/saicaca/fuwari',     // 内部链接不应包含基本路径，因为它是自动添加的
+      external: true,                               // 显示外部链接图标并将在新选项卡中打开
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/demo-avatar.png',  // 相对于/src目录。如果以“/”开头，则相对于 /public 目录
   name: 'Lorem Ipsum',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
+      icon: 'fa6-brands:twitter',       // 访问 https://icones.js.org/ 获取图标代码
+                                        // 如果尚未包含，您将需要安装相应的图标集
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://twitter.com/pilipiala',
     },
     {
       name: 'Steam',
@@ -70,7 +70,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/pilipiala',
     },
   ],
 }
