f05a960e113535c0ec3a649755228b9e14067a45
啊茶kk <acgr18share@gmail.com>
2025-01-06 14:55:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '啊茶茶のblog',
+  title: '啊茶茶のBlog',
   subtitle: 'Demo Site',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
